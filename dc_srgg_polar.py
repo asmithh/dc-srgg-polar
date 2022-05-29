b14119931f27ad48b15b6712840503d73655466a
Alyssa Smith <asmithh99@gmail.com>
2022-05-29 13:12:46 -0400

    minor comments

--- dc_srgg_polar.py
+++ dc_srgg_polar.py
@@ -38,13 +38,16 @@ def dc_srgg_polar(colors, C=5.0, little_c=2.0, gamma=2.05, beta=2.5, x_0=1.0):
         adj[colors[i]] = {}
         for j in range(i, len(colors)):
             d_i_j = np.abs(X[i] - X[j])
+            # distance parameter (in hidden variables)
             p_i_j = little_c * K[i] * K[j]
             p_i_j *= (d_i_j ** beta)
+            # degree correlation
             draw = np.random.uniform()
             if draw < p_i_j:
                 adj[colors[i]][colors[j]] = True            
+                # choosing if the edge exists
         coords[colors[i]] = (C - 3 * np.log(K[i]), 360 * X[i])
-
+        # creating coordinates
     return adj, coords
 
 colors = ['orange-tan', 'red', 'bright-yellow', 'royal-blue', 'turquoise', 'grey', 'lime-green', 'gold', 'brown', 'pink', 'light-tan', 'green', 'purple', 'orange']
